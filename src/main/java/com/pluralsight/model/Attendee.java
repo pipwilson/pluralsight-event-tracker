b0fb94161aeb951cd2f9a1696dded822c7b7651d
Phil Wilson <github@philwilson.org>
2017-01-10 14:21:04 +0000

    Add custom validation to the Attendee model

--- src/main/java/com/pluralsight/model/Attendee.java
+++ src/main/java/com/pluralsight/model/Attendee.java
@@ -1,10 +1,30 @@
 package com.pluralsight.model;
 
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.pluralsight.view.Phone;
+
 public class Attendee {
 
+	@Size(min=2, max=30)
 	private String name;
 
+	@NotEmpty @Email
 	private String emailAddress;
+
+	@Phone
+	private String phone;
+	
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
 	public String getName() {
 		return name;
